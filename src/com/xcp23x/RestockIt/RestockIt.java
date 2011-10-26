5d8fcc7e92530fb2e0975bd1839653c6f4384b7d
xCP23x <cp23@live.co.uk>
2011-10-26 14:11:22 +0100

    modified:   RestockIt.java         removed unused commented code

--- src/com/xcp23x/RestockIt/RestockIt.java
+++ src/com/xcp23x/RestockIt/RestockIt.java
@@ -1,11 +1,5 @@
 package com.xcp23x.RestockIt;
 
-/*import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;*/
 import java.util.logging.Logger;
 import org.bukkit.Material;
 import org.bukkit.event.Event;
@@ -33,62 +27,6 @@ public class RestockIt extends JavaPlugin {
     PluginManager pm = this.getServer().getPluginManager();
     pm.registerEvent(Event.Type.PLAYER_INTERACT, playerListener, Event.Priority.Normal, this);
     pm.registerEvent(Event.Type.SIGN_CHANGE, blockListener, Event.Priority.Normal, this);
-    //pm.registerEvent(Event.Type.REDSTONE_CHANGE, blockListener, Event.Priority.Normal, this);
-    
-/*    if(Bukkit.getServer().getPluginManager().isPluginEnabled("PermissionsEx")){
-        RestockIt.log.info("[RestockIt] PermissionsEX found");
-    } else {
-        RestockIt.log.info("[RestockIt] PermissionsEX not found - Using op status");
-    } */
-    
-    
-    
-/*    if (!new File("plugins/FullChest2").exists()) {
-      FullChest2.log.info("[FullChest 2] Config directory does not exist. Creating new folder...");
-      new File("plugins/FullChest2").mkdir();
-      FullChest2.log.info("[FullChest 2] Config directory created.");
-    }
-
-    if (!new File("plugins/FullChest2/config.txt").exists()) {
-      try {
-        new File("plugins/FullChest2/config.txt").createNewFile();
-        try {
-          FileWriter fw = new FileWriter("plugins/FullChest2/config.txt");
-          BufferedWriter bw = new BufferedWriter(fw);
-          bw.write("Dispenser=0");
-          bw.close();
-          fw.close();
-        }
-        catch (Exception ex) {
-          FullChest2.log.info("[FullChest 2] Error making config!");
-        }
-      } catch (IOException e) {
-      }
-
-    }
-
-    try
-    {
-      FileReader fr = new FileReader("plugins/FullChest2/config.txt");
-      BufferedReader br = new BufferedReader(fr);
-
-      if (br.readLine().equalsIgnoreCase("dispenser=1"))
-      {
-        pm.registerEvent(Event.Type.REDSTONE_CHANGE, blockListener, Event.Priority.Normal, this);
-
-        FullChest2.log.info("[FullChest 2] Dispensers are enabled (MAY BE LAGGY)");
-      }
-      else {
-        FullChest2.log.info("[FullChest 2] Dispensers are disabled");
-      }
-
-      br.close();
-      fr.close();
-    }
-    catch (Exception ex) {
-      FullChest2.log.info("[FullChest 2] Unable to read config - Dispensers Disabled");
-    }
- */
   }
         
     public boolean isInteger(String input) {
