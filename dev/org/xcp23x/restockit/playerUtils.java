56a19fe7827ae28288fe26e7ca13bde8a76a39a5
Chris Price <cp23@live.co.uk>
2012-02-25 21:40:57 +0000

    Added player message colours

--- dev/org/xcp23x/restockit/playerUtils.java
+++ dev/org/xcp23x/restockit/playerUtils.java
@@ -3,44 +3,51 @@
 package org.xcp23x.restockit;
 
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import ru.tehkode.permissions.PermissionManager;
 import ru.tehkode.permissions.bukkit.PermissionsEx;
 
 public class playerUtils {
+    static ChatColor bCol = ChatColor.DARK_AQUA; //Bracket colour
+    static ChatColor rCol = ChatColor.GRAY; // RestockIt colour
+    static ChatColor eCol = ChatColor.DARK_RED; //Error colour
+    static ChatColor dCol = ChatColor.RED;//Description colour
+    static ChatColor sCol = ChatColor.GOLD; //String colour
+    
     public static void sendPlayerMessage(Player player, int number) {
-        player.sendMessage("[RestockIt] Error " + number + ":");
+        player.sendMessage(bCol + "[" + rCol + "RestockIt" + bCol + "]" + eCol + " Error " + number + ":");
         switch(number){
             case 1:
-                player.sendMessage("This is already a RestockIt container");
+                player.sendMessage(dCol + "This is already a RestockIt container");
                 break;
             case 6:
-                player.sendMessage("No chest was found below or above the sign");
+                player.sendMessage(dCol + "No chest was found below or above the sign");
                 break;
             default:
-                player.sendMessage("Unspecified Error");
+                player.sendMessage(dCol + "Unspecified Error");
                 break;
         }
     }
     
     public static void sendPlayerMessage(Player player, int number, String string) {
-        player.sendMessage("[RestockIt] Error " + number + ":");
+        player.sendMessage(bCol + "[" + rCol + "RestockIt" + bCol + "]" + eCol + " Error " + number + ":");
         switch(number){
             case 2:
-                player.sendMessage("You do not have permission to create a RestockIt " + string);
+                player.sendMessage(dCol + "You do not have permission to create a RestockIt " + sCol + string);
                 break;
             case 3:
-                player.sendMessage(string + " is not a valid item ID");
+                player.sendMessage(sCol + string + dCol + " is not a valid item ID");
                 break;
             case 4:
-                player.sendMessage(string + " is not a valid item name");
+                player.sendMessage(sCol + string + dCol + " is not a valid item name");
                 break;
             case 5:
-                player.sendMessage(string + " is not a valid damage value");
+                player.sendMessage(sCol + string + dCol + " is not a valid damage value");
                 break;
             default:
-                player.sendMessage("Unspecified Error");
+                player.sendMessage(dCol + "Unspecified Error");
                 break;
         }
     }
