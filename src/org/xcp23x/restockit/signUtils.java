24ba5dd7b61231e29b64eeeb9a1d28504c87b043
Chris Price <cp23@live.co.uk>
2012-03-29 10:43:05 +0100

    Added GPL license test to project

--- src/org/xcp23x/restockit/signUtils.java
+++ src/org/xcp23x/restockit/signUtils.java
@@ -1,4 +1,6 @@
-//@author Chris Price (xCP23x)
+//Copyright (C) 2011-2012 Chris Price (xCP23x)
+//This software uses the GNU GPL v2 license
+//See http://github.com/xCP23x/RestockIt/blob/master/README and http://github.com/xCP23x/RestockIt/blob/master/LICENSE for details
 
 package org.xcp23x.restockit;
 
