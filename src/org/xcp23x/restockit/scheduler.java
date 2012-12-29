885857b558e21131e7c8f215e041b53c6c9755e0
xCP23x <cp23@live.co.uk>
2012-12-29 17:11:36 +0000

    Implemented debug messages from RestockIt Dev

--- src/org/xcp23x/restockit/scheduler.java
+++ src/org/xcp23x/restockit/scheduler.java
@@ -15,45 +15,58 @@ class scheduler extends RestockIt {
     private static HashMap<Block, Integer> schedules = new HashMap<Block, Integer>(); //This stores task IDs (int) with the blocks they are running on
     
     public static void startSchedule(final Block block, int period) {
-        if (schedules.get(block) == null) schedules.put(block,0); //Prepare the HashMap if it's null
-        if (schedules.get(block) != 0) return; //It's already running, don't start it again
         
-        schedules.put(block, RestockIt.plugin.getServer().getScheduler().scheduleSyncRepeatingTask(RestockIt.plugin, new Runnable() {
-            @Override
-            public void run() {
-                //If no sign is there, stop (the player may have removed it mid-count)
-                if (block.getType() != Material.WALL_SIGN && block.getType() != Material.SIGN_POST) {
-                    scheduler.stopSchedule(block);
-                    return;
+        if (schedules.get(block) == null) schedules.put(block,0); //Prepare the HashMap if it's null
+        if (schedules.get(block) == 0) { //It's not running, start it
+            RestockIt.debugSched("Schedule started for block at " + getCoords(block));
+            schedules.put(block, RestockIt.plugin.getServer().getScheduler().scheduleSyncRepeatingTask(RestockIt.plugin, new Runnable() {
+                @Override
+                public void run() {
+                    //If no sign is there, stop (the player may have removed it mid-count)
+                    if (block.getType() != Material.WALL_SIGN && block.getType() != Material.SIGN_POST) {
+                        scheduler.stopSchedule(block);
+                        RestockIt.debugSched("Sign removed, cancelling schedule at " + getCoords(block));
+                        return;
+                    }
+                    
+                    Block chest = chestUtils.getChestFromSign(block);
+                    String line2 = ((Sign)block.getState()).getLine(2);
+                    String line3 = ((Sign)block.getState()).getLine(3);
+                    
+                    if(chestUtils.getCurrentItems(signUtils.getMaterial(line2), chest) >= signUtils.getMaxItems(line3)) {
+                        RestockIt.debugSched("Container is full at " + getCoords(block));
+                        //The chest has reached its limit
+                        return;
+                    }
+                    
+                    //Add item to chest
+                    ItemStack stack = new ItemStack(signUtils.getMaterial(line2), 1);
+                    Short damage = signUtils.getDamage(line2);
+                    if (damage >= 0) stack.setDurability(damage);
+                    chestUtils.getInventory(chest).addItem(stack);
+                    RestockIt.debugSched("Added " + signUtils.getMaterial(line2).toString() + " to block at " + getCoords(block));
                 }
-                
-                Block chest = chestUtils.getChestFromSign(block);
-                String line2 = ((Sign)block.getState()).getLine(2);
-                String line3 = ((Sign)block.getState()).getLine(3);
-                
-                if(chestUtils.getCurrentItems(signUtils.getMaterial(line2), chest) >= signUtils.getMaxItems(line3)) {
-                    //The chest has reached its limit
-                    return;
-                }
-                
-                //Add item to chest
-                ItemStack stack = new ItemStack(signUtils.getMaterial(line2), 1);
-                Short damage = signUtils.getDamage(line2);
-                if (damage >= 0) stack.setDurability(damage);
-                chestUtils.getInventory(chest).addItem(stack);
-            }
             
-        },0,period));
-        if (schedules.get(block) == -1) {schedules.remove(block);} //If it fails to start, remove entry from HashMap
+            },0,period));
+            if (schedules.get(block) == -1) {
+                schedules.remove(block); //If it fails to start, remove entry from HashMap
+                RestockIt.debug("Schedule FAILED to start for block at " + getCoords(block));
+            }
+        } else RestockIt.debugSched("Tried to start schedule for block at " + getCoords(block) + ", but it's already running");
     }
     
     public static void stopSchedule(Block block) {
         if(schedules.containsKey(block)){
+            RestockIt.debugSched("Removing schedule for block at " + getCoords(block));
             int i = schedules.get(block); //Get bukkit task ID
             if (i != 0) {  //If it's 0, it's not running
                 RestockIt.plugin.getServer().getScheduler().cancelTask(i); //Cancel task
                 schedules.remove(block); //Remove entry from HashMap
             }
-        }
+        } else RestockIt.debugSched("Tried to remove schedule for block at " + getCoords(block)+ ", but it doesn't exist");
+    }
+    
+    private static String getCoords(Block block){
+        return block.getX() + "," + block.getY() + "," + block.getZ();
     }
 }
